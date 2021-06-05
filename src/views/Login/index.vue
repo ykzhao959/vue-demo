fc54b8ac967e01788bf42df0ecfbbed6bcf58814
“youkaizhao” <2570376131@qq.com>
2021-06-05 17:42:15 +0800

    构建项目、配置全局样式、路由、elementui组件

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -8,7 +8,7 @@
         <el-button type="info">信息按钮</el-button>
         <el-button type="warning">警告按钮</el-button>
         <el-button type="danger">危险按钮</el-button>
-        </el-row>
+        </el-row> 
     </div> 
 </template>
 
